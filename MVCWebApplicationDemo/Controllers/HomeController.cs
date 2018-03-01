27f14fad5d91240598b9778b83c15152f531af52
Ilya Kiyanov <ikiyanov@protonmail.com>
2018-03-02 01:41:28 +0300

    First commit with only login Razor View page

---
+++ MVCWebApplicationDemo/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVCWebApplicationDemo.Models;
+
+namespace MVCWebApplicationDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Login()
+        {
+            ViewData["Message"] = "Login page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
